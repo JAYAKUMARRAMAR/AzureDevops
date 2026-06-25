fb70b19cb73b4ed1e701e29451b05d75da7c65e7
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-25 17:38:18 +0530

    update project

--- src/main/java/com/example/currency_exchange/controller/currencyExchange.java
+++ src/main/java/com/example/currency_exchange/controller/currencyExchange.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class currencyExch
+public class currencyExchange
 {
     @GetMapping("/")
     public String geName()
